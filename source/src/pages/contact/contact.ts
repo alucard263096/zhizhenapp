50918167d23f86dc2006350e20bf1fb3a2642e25
alucard263096 <alucard263096@126.com>
2018-07-27 01:12:46 +0800

    完成发现，不过有bug

--- source/src/pages/contact/contact.ts
+++ source/src/pages/contact/contact.ts
@@ -1,14 +1,46 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams,ModalController,Content, 
+  ViewController } from 'ionic-angular';
+import { AppBase } from '../../app/app.base';
+import { StatusBar } from '@ionic-native/status-bar';
+import { CourseApi } from '../../providers/course.api';
+import { BannerApi } from '../../providers/banner.api';
+import { ViewChild } from '@angular/core';
+import { stringify } from '../../../node_modules/@angular/core/src/util';
+
 
 @Component({
   selector: 'page-contact',
-  templateUrl: 'contact.html'
+  templateUrl: 'contact.html',
+  providers:[CourseApi,BannerApi]
 })
-export class ContactPage {
-
-  constructor(public navCtrl: NavController) {
+export class ContactPage extends AppBase{
+  courselist=[];
+  current=0;
+  showmiss=false;
+  constructor(public navCtrl: NavController,public navParam:NavParams
+    ,public modalCtrl:ModalController ,public viewCtrl:ViewController,
+     public statusBar: StatusBar,
+    public courseApi: CourseApi) {
+    super(navCtrl,modalCtrl,viewCtrl, statusBar);
+    console.log(navParam);
+  
 
   }
 
+  onMyShow(){
+    this.courseApi.list({isfind:"Y"}).then((courselist)=>{
+      //alert(JSON.stringify(courselist));
+      this.courselist=courselist;
+    });
+  }
+  swipe(){
+    if(this.current+2<this.courselist.length){
+      this.current++;
+      this.showmiss=true;
+      setInterval(()=>{
+        this.showmiss=false;
+      },1000);
+    }
+  }
 }
